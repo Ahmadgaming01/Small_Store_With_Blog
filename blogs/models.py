059d69bdf49c61d5a88d27d8d216ecc8afc44883
ahmad <aalmadani111@gmail.com>
2024-01-31 23:38:13 +0100

    create models for post with taggit

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 # Create your models here.
 
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    content = models.TextField(max_length = 15000)
+    publich_date = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User , related_name = 'user_post' , on_delete = models.CASCADE)
+    tags = TaggableManager()
