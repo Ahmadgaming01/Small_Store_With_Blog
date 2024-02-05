b0946837bdff87b6e14603961e46828864cc3cc8
ahmad <aalmadani111@gmail.com>
2024-02-05 23:47:09 +0100

    create urls file for settings app , regsiter in admin.py

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Company
 # Register your models here.
+class AdminCompany (admin.ModelAdmin):
+    list_display = ('name', 'address')
+
+
+admin.site.register(Company)
+
