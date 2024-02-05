a79fbbaa9d17ef96ab9fba393fad4f3fb17478ed
ahmad <aalmadani111@gmail.com>
2024-02-05 23:42:17 +0100

    create modles for company settinfs

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Company (models.Model):
+    name = models.CharField (max_length = 100)
+    address = models.CharField(max_length = 250)
+    logo = models.ImageField(upload_to='images')
+    phone = models.CharField(max_length =  30, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
