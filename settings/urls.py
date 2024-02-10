39aad199c39e981a5840113f5cb20c953e01d862
ahmad <aalmadani111@gmail.com>
2024-02-10 16:29:40 +0100

    create func. return posts from the blog , fix problem in the base.html , fixed the problem in settings.home.html

--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from .views import get_random_post
 
 urlpatterns = [
-    
+    path('home/' , get_random_post)
 ]
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from blogs.models import Post
 # Create your views here.
+
+
+def get_random_post(request):
+    data = Post.objects.filter()[:3]
+    context = {'data':data}
+    return render(request , 'settings/home.html' ,context)
