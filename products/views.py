eb083cfdefed0aaec5f2548e9f3bea94d6e216de
ahmad <aalmadani111@gmail.com>
2024-02-11 16:13:27 +0100

    add functions for return products and brands list & detail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .models import Product , Brand , Review
+from django.views import generic
 # Create your views here.
+
+
+
+class ProductList(generic.ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+
+class ProductDetial(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+
+class BrandList(generic.ListView):
+    model = Brand
+    template_name='brands/brand_list.html'
+
+class BrandDetail(generic.ListView):
+    model = Brand
+    template_name='brands/brand_detail.html'  # One brand has many
+    # products so it should be ListView not DetailView
+
