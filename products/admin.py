e8a13f5ec560744f037902963bf3cd499cb16075
ahmad <aalmadani111@gmail.com>
2024-02-11 11:23:11 +0100

    register models in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Product , Brand , Review
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [ 'name' , 'price' , 'created_date']
+    search_fields = ['name','description']
+    list_filter = [ 'price','name']
+admin.site.register(Product)
+admin.site.register(Brand)
+admin.site.register(Review)
