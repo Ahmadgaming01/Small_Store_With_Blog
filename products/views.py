d3df47c4d40ab0f8b6ae26ad35611c2baa9189b0
ahmad <aalmadani111@gmail.com>
2024-02-21 23:39:29 +0100

    add forms.py for review , add function in views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Product , Brand , Review
 from django.views import generic
+from .forms import ReviewForm
+from django.shortcuts import redirect
 # Create your views here.
 
 
@@ -12,6 +14,20 @@ class ProductList(generic.ListView):
 class ProductDetial(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
+
+
+def add_review(request, slug):
+    product = Product.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.save()
+            return redirect('products:product_detail', slug=slug)
+    else:
+        form = ReviewForm()
+    return render(request, 'products/add_review.html', {'form': form})
 
 class BrandList(generic.ListView):
     model = Brand
